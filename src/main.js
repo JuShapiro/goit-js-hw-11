ac8b5c2ac67f01fdc8758a48791977b18fb60b73
JuShapiro <yuliashepel19@gmail.com>
2024-04-06 07:30:08 +0200

    add spinner

--- src/main.js
+++ src/main.js
@@ -8,38 +8,39 @@ import { serviceGetImage } from './js/pixabay-api';
 import { createImageMarkup } from './js/render-functions';
 
 const form = document.querySelector('.js-form');
+const loader = document.querySelector('.preloader');
 const gallery = document.querySelector('.js-list');
 
 form.addEventListener('submit', handlerSearch);
 
 function handlerSearch(event) {
-  event.preventDefault();
-
-  const inputValue = event.currentTarget.elements.query.value
-    .trim()
-    .toLowerCase();
-  //spiner.on
-  serviceGetImage(inputValue)
-    .then(data => {
-      if (!data.hits.length) {
-        gallery.innerHTML = '';
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-          backgroundColor: 'red',
-          messageColor: '#fff',
+    event.preventDefault();
+    
+    const inputValue = event.currentTarget.elements.query.value
+      .trim()
+      .toLowerCase();
+    loader.style.display = 'block';
+    serviceGetImage(inputValue)
+      .then(data => {
+        if (!data.hits.length) {
+          gallery.innerHTML = '';
+          iziToast.error({
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topRight',
+            backgroundColor: 'red',
+            messageColor: '#fff',
+          });
+          return;
+        }
+        gallery.innerHTML = createImageMarkup(data.hits);
+        const galleryList = new SimpleLightbox('.gallery a', {
+          captionDelay: 250,
         });
-        return;
-      }
-      gallery.innerHTML = createImageMarkup(data.hits);
-      const galleryList = new SimpleLightbox('.gallery a', {
-        captionDelay: 250,
-      });
-      galleryList.refresh();
-    })
-    .catch(err => console.log(err));
-  // .finally(() => )//spiner.off)
+        galleryList.refresh();
+      })
+      .catch(err => console.log(err))
+      .finally(() => loader.style.display = 'none');
 
-  event.currentTarget.reset();
+    event.currentTarget.reset();
 }
